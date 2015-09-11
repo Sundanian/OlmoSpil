29a81f9c1602957c2a4a16e85c492d16e3b403ae
Sundanian <thomas.krog.horne@gmail.com>
2015-09-11 12:50:00 +0200

    Vigtige klasser oprettet

---
+++ OlmoSpil/Server.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OlmoSpil
+{
+    class Server
+    {
+        private void KickBall(Player p, Ball b)
+        {
+        }
+    }
+}
