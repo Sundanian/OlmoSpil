a774113e1eb3bae88131b5f446b3d4fecffe4554
Skinke <mer785@hotmail.com>
2015-09-15 12:32:11 +0200

    Nuuu sku det virke

--- OlmoSpil/Game1.cs
+++ OlmoSpil/Game1.cs
@@ -38,6 +38,51 @@ namespace OlmoSpil
             set { Game1.allObjects = value; }
         }
 
+        private static bool powerUpSpawned = false;
+
+        public static bool PowerUpSpawned
+        {
+            get { return Game1.powerUpSpawned; }
+            set { Game1.powerUpSpawned = value; }
+        }
+
+
+        private static bool powerUpSpawned = false;
+
+        public static bool PowerUpSpawned
+        {
+            get { return Game1.powerUpSpawned; }
+            set { Game1.powerUpSpawned = value; }
+        }
+
+
+        private static bool powerUpSpawned = false;
+
+        public static bool PowerUpSpawned
+        {
+            get { return Game1.powerUpSpawned; }
+            set { Game1.powerUpSpawned = value; }
+        }
+
+
+        private static bool powerUpSpawned = false;
+
+        public static bool PowerUpSpawned
+        {
+            get { return Game1.powerUpSpawned; }
+            set { Game1.powerUpSpawned = value; }
+        }
+
+
+        private static bool powerUpSpawned = false;
+
+        public static bool PowerUpSpawned
+        {
+            get { return Game1.powerUpSpawned; }
+            set { Game1.powerUpSpawned = value; }
+        }
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
