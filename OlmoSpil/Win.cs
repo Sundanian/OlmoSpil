18610b27b793b5f88248e36b8ba2b75719aaa141
Nikolaj Sloth <keysar50@hotmail.com>
2015-09-17 13:07:46 +0200

    WIN

---
+++ OlmoSpil/Win.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OlmoSpil
+{
+    class Win : GameObject
+    {
+
+
+        public Win(Vector2 position, int frames)
+            : base(position, frames)
+        {
+
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            HandleInput(Keyboard.GetState());
+        }
+        private void HandleInput(KeyboardState keystate)
+        {
+            if (keystate.IsKeyDown(Keys.Escape) || keystate.IsKeyDown(Keys.Back))
+            {
+                Game1.RoomChange = true;
+                Game1.currentScreen.Type = "Lobby";
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(Game1.Sf, "Player {0} Won the game! Press Esc to return to the lobby", new Vector2(Game1.Graphics.GraphicsDevice.Viewport.Width / 2, Game1.Graphics.GraphicsDevice.Viewport.Height / 2), Color.Red);
+        }
+    }
+}
